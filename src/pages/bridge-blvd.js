5bb021fb2e2b6328fdf6e33d5e54de33e36bd37a
Dean McCormack <deanmccormack@gmail.com>
2019-10-24 12:00:50 -0400

    min tablet width

--- src/pages/bridge-blvd.js
+++ src/pages/bridge-blvd.js
@@ -11,19 +11,38 @@ const BridgeBlvd = () => (
     <article>
       <h3>Bridge Blvd.</h3>
       <section style={{maxWidth: '700px'}}>
-
-        <p></p>
-
-        <div className='float-image-container'>
-          <AppLaunchImage />
-        </div>
-
-        <p></p>
-        <p></p>
+      <div className='float-image-container'>
+        <AppLaunchImage />
+      </div>
+      <section>
+        <h6>WIP - Work in Progress</h6>
+        <p>Currently working towards some visualized end. (see goal) </p>
+      </section>
+      <section>
+        <h6>Personal Project</h6>
+        <p>If thing A only needs to be 50% complete, to get thing B to 90% complete.
+        It is entirely at the discretion of? Wouldn&apos;t be cool if? (see personal project)
+        </p>
+      </section>
+      <section>
+        <h6>Goal</h6>
+        <p>Build a self-generating Rubber Bridge tutorial.</p>
+      </section>
+      <section>
+        <h6>Up Next</h6>
+        <p>
+          <ol>
+            <li>Generate a bidding cheat sheet for the configured bid system</li>
+            <li>Add SAYC</li>
+            <li>fix bugs in tablet display</li>
+            <li>way out... complete card play</li>
+          </ol>
+        </p>
+        </section>
 
         <button
           className="launch-btn"
-          title="Opens Bridge Blvd app in new tab" 
+          title="Opens Bridge Blvd app in new tab"
           onClick={
             (e) => {e.preventDefault(); window.open('https://heuristic-johnson-16785e.netlify.com/');}
         }>Launch</button>
